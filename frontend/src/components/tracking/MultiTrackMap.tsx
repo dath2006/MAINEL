692fa408f5af27735213221130f14192ece69dd2
Sathish Dath <sathishdutt0@gmail.com>
2026-01-18 16:30:13 +0530

    map fix

--- frontend/src/components/tracking/MultiTrackMap.tsx
+++ frontend/src/components/tracking/MultiTrackMap.tsx
@@ -355,8 +355,8 @@ export default function MultiTrackMap({
                 <MapEvents onMapClick={onMapClick} />
                 <MapBoundsController sources={sources} activeTracks={mergedTracks} />
                 <TileLayer
-                    attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-                    url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
                 {/* Camera Markers */}
